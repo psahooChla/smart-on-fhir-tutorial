3bc4fd481346516d30e42c784238fc10e08b6235
psahooChla <psahoo@chla.usc.edu>
2025-08-28 01:42:38 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -27,20 +27,19 @@
                       }
                     }
                   });
-
-        $.when(pt, obv).fail(onError);
-
+        
         var device = smart.patient.api.fetchAll({
           type: "Device",
         });
         
         $.when(pt, obv, device).fail(onError);
-        $.when(pt, obv, device).done(function (patient, obv, device) { console.log("Device ", device);
                                                                       
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv,device).done(function(patient, obv, device) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-
+          
+          console.log("DEVICE", device);
+          
           var fname = '';
           var lname = '';
 
