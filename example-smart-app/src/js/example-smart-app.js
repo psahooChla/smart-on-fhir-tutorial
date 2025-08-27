32639901ccb00b052f7246666ffad11f5b03f328
psahooChla <psahoo@chla.usc.edu>
2025-08-27 23:49:27 +0530

    updated the loinc code for blood pressure

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -15,9 +15,12 @@
                     type: 'Observation',
                     query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        $or: ['http://loinc.org|8302-2', // body height
+                              //'http://loinc.org|8462-4', // diastolic blood pressure (DSTU2)
+                              //'http://loinc.org|8480-6', // systolic blood pressure (DSTU2)
+                              'http://loinc.org|2085-9', // cholesterol in HDL
+                              'http://loinc.org|2089-1', // cholesterol in LDL
+                              'http://loinc.org|85354-9'] // BP pnl w all optional R4
                       }
                     }
                   });
